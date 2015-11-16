ef341320ce338f5d09c2b75821c3689c238fe3cc
Abraham <abraham@elmahrek.com>
2015-11-15 17:44:25 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,204 @@
+/*
+ * nconf-leveldb.js: nconf configuration leveldb backend
+ *
+ * (C) 2015, Abraham Elmahrek.
+ *
+ */
+
+var nconf = require('nconf');
+var level = require('level');
+var deasync = require('deasync');
+
+var Level = function(options) {
+  options       = options || {};
+  this.type     = 'level';
+  this.store    = {};
+  this.readOnly = false;
+  this.path = options.path || null;
+  this.logicalSeparator = options.logicalSeparator || ':';
+
+  this.store = level(this.path);
+};
+
+Level.prototype.get = function(key) {
+  var path   = nconf.path(key, this.logicalSeparator),
+      result = null,
+      err    = null;
+  this.store.get(path, function(_err, _value) {
+    if (err) err = _err;
+    else result = _value;
+  });
+  deasync.loopWhile(function() {
+    return result === null && err === null;
+  });
+  if (err) {
+    throw err;
+  }
+  if (result) return JSON.parse(decodeURIComponent(result));
+  else return null;
+};
+
+Level.prototype._put = function(key, value) {
+  var path = (key.length === 0) ? [] : nconf.path(key, this.logicalSeparator),
+      batch = this.store.batch(),
+      done = false,
+      err = null;
+  // Reset should be object.
+  if (path.length === 0 && (!value || typeof value !== 'object')) {
+    return false;
+  }
+
+  var helper = function(path, value) {
+    // Objects need merging only.
+    if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
+      var keys = Object.keys(value);
+      for (var i = 0; i < keys.length; ++i) {
+        var key = keys[i];
+        path.push(key);
+        helper(path, value[key]);
+        path.pop();
+      }
+    } else {
+      batch.put(nconf.key(path), encodeURIComponent(JSON.stringify(value)));
+    }
+  };
+
+  helper(path, value);
+
+  batch.write(function(_err) {
+    err = _err;
+    done = true;
+  });
+  deasync.loopWhile(function() {
+    return !done;
+  });
+  if (err) {
+    throw err;
+  }
+  return true;
+};
+
+Level.prototype.set = function(key, value) {
+  if (this.readOnly) {
+    return false;
+  }
+
+  // Overwrite key and write data.
+  return this.clear(key) && this._put(key, value);
+};
+
+Level.prototype.clear = function(key) {
+  if (this.readOnly) {
+    return false;
+  }
+
+  var path = (key.length === 0) ? [] : nconf.path(key, this.logicalSeparator),
+      newkey = nconf.key(path),
+      batch = this.store.batch(),
+      done = false,
+      err = null,
+      errFn = function(_err) {
+        err = _err;
+        done = true;
+      };
+  
+  this.store.createReadStream()
+  .on('data', function(data) {
+    if (newkey.length === 0 || newkey.startsWith(data.key)) {
+      batch.del(data.key);
+    }
+  })
+  .on('error', errFn)
+  .on('end', function() {
+    batch.write(errFn);
+  });
+  deasync.loopWhile(function() {
+    return !done;
+  });
+  if (err) {
+    throw err;
+  }
+  return true;
+};
+
+Level.prototype.merge = function(key, value) {
+  if (this.readOnly) {
+    return false;
+  }
+
+  // Objects need merging only.
+  if (typeof value !== 'object' || Array.isArray(value) || value === null) {
+    return this.set(key, value);
+  }
+
+  return this._put(key, value);
+};
+
+Level.prototype.reset = function() {
+  if (this.readOnly) {
+    return false;
+  }
+
+  var self = this,
+      done = false,
+      err = null,
+      errFn = function(cb) {
+        return function(_err) {
+          if (_err) {
+            err = _err;
+            done = true;
+          } else {
+            return cb(Array.prototype.slice.call(arguments, 1));
+          }
+        };
+      };
+  this.store.close(errFn(function(_err) {
+    level.destroy(self.path, errFn(function() {
+      self.store = level(self.path);
+      done = true;
+    }));
+  }));
+  deasync.loopWhile(function() {
+    return !done;
+  });
+  if (err) {
+    throw err;
+  }
+  return true;
+};
+
+Level.prototype.loadSync = function() {
+  var result = {},
+      done = false,
+      err = null;
+  this.store.createReadStream()
+  .on('data', function(data) {
+    var path = nconf.path(key, this.logicalSeparator),
+        lastKey = path.pop(),
+        target = result;
+    while (path.length > 0) {
+      var key = path.shift();
+      if (!(key in target)) target[key] = {};
+      target = target[key];
+    }
+    target[lastKey] = data.value;
+  })
+  .on('error', function(_err) {
+    err = _err;
+    done = true;
+  })
+  .on('end', function() {
+    done = true;
+  });
+  deasync.loopWhile(function() {
+    return !done;
+  });
+  if (err) {
+    throw err;
+  }
+  return result;
+};
+
+module.exports = function(provider) {
+  provider.Level = Level;
+};
---
+++ test/main-test.js
@@ -0,0 +1,60 @@
+var assert = require("assert"),
+    fs = require("fs");
+
+describe("#full()", function () {
+  var nconf = require("nconf");
+
+  before(function() {
+    require("../index.js")(nconf);
+    nconf.use("level", {
+      path: "./testdb"
+    });
+    nconf.reset();
+  });
+
+  after(function() {
+    nconf.reset();
+  });
+
+  it("literals", function () {
+    nconf.set("number", 1);
+    nconf.set("string", "test");
+    nconf.set("float", 1.2);
+    nconf.set("boolean-true", true);
+    nconf.set("boolean-false", false);
+    nconf.set("array", [1, 2, 3]);
+    assert.equal(nconf.get("number"), 1);
+    assert.equal(nconf.get("string"), "test");
+    assert.equal(nconf.get("float"), 1.2);
+    assert.equal(nconf.get("boolean-true"), true);
+    assert.equal(nconf.get("boolean-false"), false);
+    assert.deepEqual(nconf.get("array"), [1, 2, 3]);
+
+    // Nested
+    nconf.set("n:number", 1);
+    nconf.set("n:string", "test");
+    nconf.set("n:float", 1.2);
+    nconf.set("n:boolean-true", true);
+    nconf.set("n:boolean-false", false);
+    assert.equal(nconf.get("n:number"), 1);
+    assert.equal(nconf.get("n:string"), "test");
+    assert.equal(nconf.get("n:float"), 1.2);
+    assert.equal(nconf.get("n:boolean-true"), true);
+    assert.equal(nconf.get("n:boolean-false"), false);
+  });
+
+  it("objects", function () {
+    nconf.set("1:2:3:4", {"5": {"6": 1}});
+    assert.equal(nconf.get("1:2:3:4:5:6"), 1);
+
+    // Test root.
+    nconf.set("", {"5": {"6": 1}});
+    assert.equal(nconf.get("5:6"), 1);
+    assert.equal(nconf.get("1:2:3:4:5:6"), null);
+  });
+
+  it("nulls", function () {
+    nconf.set("null", null);
+    assert.equal(nconf.get("null"), null);
+  });
+});
